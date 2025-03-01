e8f664b65f09a3a353149840737b36e3bbdaeb5e
RenataSz4 <131920616+RenataSz4@users.noreply.github.com>
2025-03-01 11:39:14 -0800

    Add files via upload
    Aplicación final

---
+++ Proyecto1/Proyecto1/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  Proyecto1
+//
+//  Created by CETYS Universidad  on 19/02/25.
+//
+
+import SwiftUI
+
+struct Location : Identifiable {
+    let id = UUID()
+    let latitude: Double
+    let longitude: Double
+}
---
+++ Proyecto1/Proyecto1/View/RouteListView.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+struct RouteListView: View {
+    
+    @StateObject private var viewModel = RouteViewModel()
+    @State private var showRouteStartView = false
+    @State private var selectedRoute: Route?
+    
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.routes) { route in
+                    HStack {
+                        NavigationLink(
+                            destination: RouteView(route: route)
+                        ) {
+                            RouteRowView(route: route)
+                        }
+                    }
+                    .swipeActions {
+                        // Delete
+                        Button(role: .destructive) {
+                            if let index = viewModel.routes.firstIndex(where: { $0.id == route.id }) {
+                                viewModel.routes.remove(at: index)
+                            }
+                        } label: {
+                            Label("Delete", systemImage: "trash")
+                        }
+
+                        // Edit
+                        Button {
+                            selectedRoute = route
+                            
+                        } label: {
+                            Label("Edit", systemImage: "pencil")
+                        }
+                        .tint(.blue)
+                    }
+                }
+            }
+            .navigationTitle("Routes")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showRouteStartView = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showRouteStartView) {
+                RouteStartView(showRouteStartView: $showRouteStartView)
+                    .environmentObject(viewModel)
+            }
+            .sheet(item: $selectedRoute) { route in
+                RouteEditView(route: $selectedRoute, showRouteStartView: .constant(false))
+                    .environmentObject(viewModel)
+            }
+
+        }
+    }
+}
+
+#Preview {
+    RouteListView()
+}
+
